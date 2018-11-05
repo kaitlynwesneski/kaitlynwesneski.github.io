c568e13741b04ff2590fc8f8ff12a3936ae96bf8
Kaitlyn Wesneski <klw3101@ppsbstudent.org>
2018-11-05 23:59:38 +0000

    installed circularity

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -35,10 +35,10 @@
         platform.create(0, game.world.height - 32, 3, 2);
 
         // example:
-        platform.create(400, 460);
-        platform.create(500, 200);
-        platform.create(600, 50);
-        
+        platform.create(300, 200);
+        platform.create(400, 300);
+        platform.create(500, 400);
+        platform.create(250, 550);
         
         
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
